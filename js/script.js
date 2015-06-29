4473f5cae38b90563f09a8977f909012d56fd1f1
jfisher1995 <jfisher1995@gmail.com>
2015-06-29 16:23:09 -0600

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 $(function(){
     console.log('jQuery loaded and running');
 });
+
+$(".wireframes").load("svg/UX/Wireframes.svg");
+$(".interactive").load("svg/UX/InteractivePrototypes.svg");
+$(".eval").load("svg/UX/HeuristicEvaluation.svg");
+$(".usability").load("svg/UX/UsabilityTesting.svg");
+$(".user").load("svg/UX/UserPersonas.svg");
+
+$(".svg").click(function(){
+	$(this).children().attr("state", "play");
+	$(this).children().children().attr("state", "play");
+	$(this).children("p").show();
+	console.log("hover");
+});
