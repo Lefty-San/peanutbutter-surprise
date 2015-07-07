7b7c9f16bb670b7e309b74e638d143a8ac61e877
jfisher1995 <jfisher1995@gmail.com>
2015-07-07 10:23:54 -0600

    Super cool animations added

--- js/script.js
+++ js/script.js
@@ -3,30 +3,50 @@ $(function(){
     console.log('jQuery loaded and running \n pixels available in document window ' + window.innerHeight);
 });
 
-//slider logic
-var current=1;
-$(".num"+current).children().addClass("large");
-$(".leftbutt").css('visibility','hidden');
-$(".leftButt").click(function(){
-	console.log("clicked");
+//functions
+function moveCirc(curr, next){
+	TweenLite.to($(".num"+curr).children(), .5, {
+		transform: "scale(1, 1)",
+		transformOrigin: "50% 50%",
+		ease:Power2.easeInOut
+	});
+	TweenLite.to($(".num"+next).children(), .5, {
+		transform: "scale(2, 2)",
+		transformOrigin: "50% 50%",
+		ease:Power2.easeInOut
+	});
+}
+
+function norm(img){
+	TweenMax.to( $(img), .3, {
+		transform: "scale(1, 1) translateX(0px)",
+		opacity: "1",
+		transformOrigin:"50% 50%",
+		ease:Power2.easeInOut,
+	});
+}
+
+function moveLeft(){
 	$(".final").addClass("pixPerf").removeClass("final");
 	$(".design").addClass("final").removeClass("design");
 	$(".sol").addClass("design").removeClass("sol");
 	$(".ux").addClass("sol").removeClass("ux");
 	$(".us").addClass("ux").removeClass("us");
 	$(".projectSpecs").addClass("us").removeClass("projectSpecs");
+
+	norm("#image");
+
 	if ($("#image").hasClass("pixPerf")){
 		$(".leftButt").css('visibility','hidden');
 	}
 	$(".rightButt").css('visibility','visible');
-
 	if (current != 1){
 		current--;
 	}
-	$(".num"+current).children().addClass("large");
-	$(".num"+(current+1)).children().removeClass("large")
-});
-$(".rightButt").click(function(){
+	moveCirc(current+1, current);
+}
+
+function moveRight(){
 	console.log("clicked");
 	$(".us").addClass("projectSpecs").removeClass("us");
 	$(".ux").addClass("us").removeClass("ux");
@@ -34,6 +54,9 @@ $(".rightButt").click(function(){
 	$(".design").addClass("sol").removeClass("design");
 	$(".final").addClass("design").removeClass("final");
 	$(".pixPerf").addClass("final").removeClass("pixPerf");
+
+	norm("#image");
+
 	if ($("#image").hasClass("projectSpecs")){
 		$(".rightButt").css('visibility','hidden');
 	}
@@ -42,15 +65,58 @@ $(".rightButt").click(function(){
 	if (current != 7){
 		current++;
 	}
-	$(".num"+current).children().addClass("large");
-	$(".num"+(current-1)).children().removeClass("large")
+	moveCirc(current-1, current);
+}
+
+function animateRight(){
+	TweenMax.to( $(".rightButt"), .1, {
+		transform: "scale(.75, .75) translateX(5px)", 
+		transformOrigin:"50% 50%", 
+		ease:Power2.easeInOut,
+		repeat:1,
+		yoyo: true
+	});
+	TweenMax.to( $("#image"), .3, {
+		transform: "scale(.9, .9) translateX(15px)",
+		opacity: ".26",
+		transformOrigin:"50% 50%",
+		ease:Power2.easeInOut,
+		onComplete: moveRight
+	});
+}
+function animateLeft(){
+	TweenMax.to( $(".leftButt"), .1, {
+		transform: "scale(.75, .75) translateX(-5px)", 
+		transformOrigin:"50% 50%", 
+		ease:Power2.easeInOut,
+		repeat:1,
+		yoyo: true,
+	});
+	TweenMax.to( $("#image"), .3, {
+		transform: "scale(.9, .9) translateX(-15px)",
+		opacity: ".26",
+		transformOrigin:"50% 50%",
+		ease:Power2.easeInOut,
+		onComplete: moveLeft
+	});
+}
+
+//slider logic
+current=1;
+moveCirc(1,1);
+$(".leftbutt").css('visibility','hidden');
+$(".leftButt").click(function(){
+	animateLeft();
+});
+$(".rightButt").click(function(){
+	animateRight();
 });
 
 //hide x until needed
 $(".x").hide();
 
 //hide menu until clicked
-var menuHidden=true;
+menuHidden=true;
 $(".menu ul").hide();
 $(".menu .image").click(function(){
 	if (menuHidden == false){
@@ -91,22 +157,56 @@ $(".poc").prepend($('<div>').load("svg/UI/POC.svg"));
 $("[state='small']").click(function(e){
   $(this).attr("state", "bigAF");
   if ($(this).hasClass("wireframes")||$(this).hasClass("mobile")){
-  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {transform: "scale(10,10) translateX(10px)", zIndex: 1, transformOrigin:"50% 50%", ease:Power2.easeInOut});
+  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {
+  	  	transform: "scale(10,10) translateX(10px)", 
+  	  	zIndex: 1, 
+  	  	transformOrigin:"50% 50%", 
+  	  	ease:Power2.easeInOut});
   }
   if ($(this).hasClass("proto")||$(this).hasClass("desktop")){
-  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {transform: "scale(10,10) translateX(-7px)", zIndex: 1, transformOrigin:"50% 50%", ease:Power2.easeInOut});
+  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {
+  	  	transform: "scale(10,10) translateX(-7px)",
+  	  	 zIndex: 1, 
+  	  	 transformOrigin:"50% 50%", 
+  	  	 ease:Power2.easeInOut
+  	  	});
   }
   if ($(this).hasClass("user")||$(this).hasClass("poc")){
-  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {transform: "scale(10,10) translateY(-15px) translateX(8px)", zIndex: 1, transformOrigin:"50% 50%", ease:Power2.easeInOut});
+  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {
+  	  	transform: "scale(10,10) translateY(-15px) translateX(8px)", 
+  	  	zIndex: 1, 
+  	  	transformOrigin:"50% 50%", 
+  	  	ease:Power2.easeInOut
+  	  });
   }
   if ($(this).hasClass("eval")||$(this).hasClass("presen")){
-  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {transform: "scale(10,10) translateY(-15px) translateX(-8px)", zIndex: 1, transformOrigin:"50% 50%", ease:Power2.easeInOut});
+  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {
+  	  	transform: "scale(10,10) translateY(-15px) translateX(-8px)", 
+  	  	zIndex: 1, 
+  	  	transformOrigin:"50% 50%", 
+  	  	ease:Power2.easeInOut
+  	  });
   }
   if ($(this).hasClass("usability")||$(this).hasClass("custom")){
-  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {transform: "scale(10,10) translateY(-30px)", zIndex: 1, transformOrigin:"50% 50%", ease:Power2.easeInOut});
+  	  TweenLite.to( $("[state='bigAF'] div svg"), 2, {
+  	  	transform: "scale(10,10) translateY(-30px)", 
+  	  	zIndex: 1, 
+  	  	transformOrigin:"50% 50%", 
+  	  	ease:Power2.easeInOut
+  	  });
   }
-  TweenLite.to("[state='bigAF'] div svg g#Shadow", 2, {opacity:"0", transformOrigin:"50% 50%", zIndex: 1,ease:Power2.easeInOut});
-  TweenLite.to("[state='bigAF'] div svg g#icon", 2, {transform: "translateY(-70px) translateZ(0) scale(0.25, 0.25)", zIndex: 1,transformOrigin:"50% 50%", ease:Power2.easeInOut});
+  TweenLite.to("[state='bigAF'] div svg g#Shadow", 2, {
+  	opacity:"0", 
+  	transformOrigin:"50% 50%", 
+  	zIndex: 1,
+  	ease:Power2.easeInOut
+  });
+  TweenLite.to("[state='bigAF'] div svg g#icon", 2, {
+  	transform: "translateY(-70px) translateZ(0) scale(0.25, 0.25)", 
+  	zIndex: 1,
+  	transformOrigin:"50% 50%", 
+  	ease:Power2.easeInOut
+  });
   $(".x").show();
 });
 $(".x").click(function(){
