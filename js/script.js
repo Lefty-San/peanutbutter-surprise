a00d5e78acfcabc01aec248b23e52f470f811808
jfisher1995 <jfisher1995@gmail.com>
2015-07-14 13:11:32 -0600

    Added page 5 and footer, fixed alignment errors on pagination

--- js/script.js
+++ js/script.js
@@ -21,45 +21,45 @@ device = $('body').attr("class");
 //console.log(device);
 
 $.fn.isOnScreen = function(x, y){
-
+    
     if(x == null || typeof x == 'undefined') x = 1;
     if(y == null || typeof y == 'undefined') y = 1;
-
+    
     var win = $(window);
-
+    
     var viewport = {
         top : win.scrollTop(),
         left : win.scrollLeft()
     };
     viewport.right = viewport.left + win.width();
     viewport.bottom = viewport.top + win.height();
-
+    
     var height = this.outerHeight();
     var width = this.outerWidth();
-
+ 
     if(!width || !height){
         return false;
     }
-
+    
     var bounds = this.offset();
     bounds.right = bounds.left + width;
     bounds.bottom = bounds.top + height;
-
+    
     var visible = (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-
+    
     if(!visible){
-        return false;
+        return false;   
     }
-
+    
     var deltas = {
         top : Math.min( 1, ( bounds.bottom - viewport.top ) / height),
         bottom : Math.min(1, ( viewport.bottom - bounds.top ) / height),
         left : Math.min(1, ( bounds.right - viewport.left ) / width),
         right : Math.min(1, ( viewport.right - bounds.left ) / width)
     };
-
+    
     return (deltas.left * deltas.right) >= x && (deltas.top * deltas.bottom) >= y;
-
+    
 };
 
 //  condensed slider functions
@@ -79,15 +79,15 @@ TweenMax.set($('.svgs'),{perspective:1000});
 //functions
 function makeBigAF(translate, art){
 	var TIMING = 1.5;
-	TweenLite.to("[state='bigAF'] div svg path:not(g path)", TIMING, {
+	TweenLite.to("[state='bigAF'] div svg g#Shadow", TIMING, {
 	  	opacity:"0",
 	  	transformOrigin:"50% 50%",
 	  	zIndex: 2,
 	  	ease:Power2.easeInOut
 	});
 
-	if(device == "smartPhone"){
-		TweenLite.to("[state='bigAF'] div svg g", TIMING, {
+	if(device == "smartPhone"){		
+		TweenLite.to($("[state='bigAF'] div svg g#icon"), TIMING, {
 		  	transform: "translateY(-70px) translateZ(0) scale(0.25, 0.25)",
 		  	zIndex: 3,
 		  	transformOrigin:"50% 50%",
@@ -101,7 +101,7 @@ function makeBigAF(translate, art){
 	  	});
 	}
 	else if(device == "tabletPort"){
-		TweenLite.to("[state='bigAF'] div svg path:not(g path)", TIMING, {
+		TweenLite.to($("[state='bigAF'] div svg g"), TIMING, {
 		  	transform: "translateY(-200px) translateX(5px) translateZ(0) scale(.5,.5)",
 		  	zIndex: 3,
 		  	transformOrigin:"50% 50%",
@@ -199,7 +199,6 @@ function animateRight(){
 		onComplete: moveRight
 	});
 }
-
 function animateLeft(){
 	TweenMax.to( $(".leftButt"), .1, {
 		transform: "scale(.75, .75) translateX(-5px)",
